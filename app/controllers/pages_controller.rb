9fe5456b70d6c8800b45b38a07d9ef79697e766d
Shlomi Zadok <shlomi@linnovate.net>
2010-12-16 16:16:15 +0200

    1st commit & user model phase 1

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home
+    @title = "The home of studyrs"
+  end
+
+  def about
+    @title = "About Us"
+  end
+
+  def contact
+    @title = "Contact Us"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studyr::Application.config.secret_token = 'f80ff09c445b3489e390e20e808ccd0be85938d38d424e0022e4b46abc8d3a5e9497010315635873b5ba352baa6cc53c1e1215602f33327fe801bca335e7ad26'

c49f07e416774e7a21959b9e0b9fb7fcd7326e88
Shlomi Zadok <shlomi@ben-hanna.com>
2011-09-04 11:11:21 +0300

    Changes in production.rb to support serving assets, second try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,6 @@ Studyr::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
-  config.assets.compress = true
-  config.assets.compile = true
   
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
@@ -19,7 +17,7 @@ Studyr::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true

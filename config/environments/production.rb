44a4afd3a5b20ba98e917583a0f6abb7e8a8be86
Shlomi Zadok <shlomi@ben-hanna.com>
2011-09-04 11:06:36 +0300

    Changes in production.rb to support serving assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,10 @@ Studyr::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
+  config.assets.compress = true
+  config.assets.compile = true
+  
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # Compress JavaScripts and CSS
   config.assets.compress = true

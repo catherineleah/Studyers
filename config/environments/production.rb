616b12a56336e9d33d0d343a10fd63b63fa371cc
Shlomi Zadok <shlomi@ben-hanna.com>
2011-11-12 22:57:41 +0200

    production images test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Studyr::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   
   #config.action_dispatch.x_sendfile_header = "X-Sendfile"
   config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"

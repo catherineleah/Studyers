7d662e95794fb2ba5c89dd76e296a460fe8eb0d2
Ubuntu <ubuntu@ip-10-244-153-73.ec2.internal>
2011-11-24 14:03:03 +0000

    schema issues

--- db/schema.rb
+++ db/schema.rb
@@ -20,12 +20,14 @@ ActiveRecord::Schema.define(:version => 20111022143129) do
     t.datetime "updated_at"
   end
 
-  create_table "images", :force => true do |t|
-    t.text     "image"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "lesson_id"
+  create_table "friendships", :force => true do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.integer "blocker_id"
+    t.boolean "pending",    :default => true
   end
+
+  add_index "friendships", ["user_id", "friend_id"], :name => "index_friendships_on_user_id_and_friend_id", :unique => true
 
   create_table "lessons", :force => true do |t|
     t.string   "title"
@@ -48,17 +50,10 @@ ActiveRecord::Schema.define(:version => 20111022143129) do
 
   add_index "notebooks", ["user_id"], :name => "index_notebooks_on_user_id"
 
-  create_table "presentaions", :force => true do |t|
-    t.text     "slide"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "presentations", :force => true do |t|
     t.text     "slide"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "lesson_id"
   end
 
   create_table "shares", :force => true do |t|
@@ -66,13 +61,6 @@ ActiveRecord::Schema.define(:version => 20111022143129) do
     t.string   "shared_ids"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "texts", :force => true do |t|
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "lesson_id"
   end
 
   create_table "users", :force => true do |t|

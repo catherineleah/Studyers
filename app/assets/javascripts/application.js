dafe96e536fad3e18bbe9817f3044eff34c8a1af
Shlomi Zadok <shlomi@ben-hanna.com>
2011-12-24 01:51:12 +0200

    fixing js?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,7 @@
 //= require jquery  
 //= require jquery-ui
 //= require jquery_ujs
-//= require jquery.poshytip
-//= require jquery.watermark
+
 //= require_self  
 
 $(document).ready(function() {

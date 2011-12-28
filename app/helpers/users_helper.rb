a78cbadf238ab92d45f97f55599ec9aab6933ac7
Shlomi Zadok <shlomi@ben-hanna.com>
2011-12-28 22:16:16 +0200

    fixed helper to show default icon

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def gravatar_for(user, options = {})
     options = { :size => 120 }.merge(options)
-    options[:default] = image_path("http://localhost:3000/assets/default_icon.jpg")
+    options[:default] = image_path("http://studyers.com/assets/default_icon.jpg")
     gravatar_image_tag(user.email.downcase, :alt => user.name,
                                             :class => 'gravatar',
                                             :gravatar => options)
